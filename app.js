4275742b5ee8018e22db9f7839cc28e65689032a
Gabiest <gabrielnetolobo@gmail.com>
2024-07-05 16:42:08 -0300

    commit final

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 alert('Bem-vindo ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 
     if (chute == numeroSecreto) {
-        alert(`Você descobriu o número secreto ${numeroSecreto} com um total de ${tentativas}`);
+        break;
     } else {
-        if (chute < numeroSecreto) {
-            alert(`o número secreto é maior que ${chute}`);
-        } else {
+        if (chute > numeroSecreto) {
             alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
         }
         tentativas++
     };
-} 
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentivas' : 'tentativa'
+alert(`Você descobriu o número secreto ${numeroSecreto} com um total de ${tentativas} ${palavraTentativa}`);
